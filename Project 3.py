645e4a2350e9b2072c0e321e3b3faa5956871fc0
Pohad <oscar.pohad@gmail.com>
2017-10-09 12:31:44 +0200

    Update Project 3.py

--- Project 3.py
+++ Project 3.py
@@ -8,15 +8,26 @@ from pylab import *
 import sys    
 
 class FourVector:
-    def __init__(self,t,x,y,z):
-        self.t=t
-        self.x=x
-        self.y=y
-        self.z=z
-        self.coordvec=array([[self.t],[self.x],[self.y],[self.z]])
+    def __init__(self,txyz):
+        if isinstance(txyz,ndarray):
+            txyz.shape=(4,1)
+            self.t=txyz[0,]
+            self.x=txyz[1,]
+            self.y=txyz[2,]
+            self.z=txyz[3,]
+            self.coordvec=txyz
+        elif isinstance(txyz,(tuple,list)):
+            self.t=txyz[0]
+            self.x=txyz[1]
+            self.y=txyz[2]
+            self.z=txyz[3]
+            self.coordvec=array([[txyz[0]],[txyz[1]],[txyz[2]],[txyz[3]]])
+        else:
+            raise TypeError('Input must be array, tuple, or list')
         
     def __repr__(self):
-        return'{}'.format(array([[self.t],[self.x],[self.y],[self.z]]))        
+        return'{}'.format(self.coordvec)        
+
         
     def MinkProd(self, other):
         if isinstance(other, ndarray):
